ae57cf6cb2c7601de609f645ecaaba2ed6429a89
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2026-05-19 20:57:33 +0700

    test2wqeưqe2qw

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-pkill java || true
-
-nohup java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+nohup java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/app.log 2>&1 &
