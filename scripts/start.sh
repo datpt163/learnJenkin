ee16ff2038f4eef16ccd741b6399e3b119d109a8
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2026-05-19 21:14:16 +0700

    test2wqeưqe2qwấ

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -jar /home/ubuntu/app/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/app.log 2>&1 &
+nohup java -jar /home/ubuntu/app/target/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/app.log 2>&1 &
